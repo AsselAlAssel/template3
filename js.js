92d74188d06dbe16224d5cbe3053c8d38072e118
assel al assel <aseel1234500000@gmail.com>
2022-09-08 20:06:35 +0300

    add stats section and style it

--- js.js
+++ js.js
@@ -1,17 +1,39 @@
 const skillsLine = document.querySelectorAll('.skills--progressPercent');
 const dataProgroessLine = document.querySelectorAll('.skills--progressLine');
+const stats = document.querySelector('.stats');
+const statsNumber = document.querySelectorAll('.stats--card .card--number');
+let started = false;
 
+const counter = (temp) => {
+    const goal = temp.dataset.goal;
+    console.log(goal);
+    const count = setInterval(() => {
+        temp.textContent++;
+        if (temp.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
 
+}
 
 document.addEventListener('scroll', function (e) {
     if (skillsLine[0].getBoundingClientRect().top < 500) {
-        skillsLine[0].style.width = dataProgroessLine[0].dataset.percent1;
-        skillsLine[1].style.width = dataProgroessLine[1].dataset.percent2;
-        skillsLine[2].style.width = dataProgroessLine[2].dataset.percent3;
-        skillsLine[3].style.width = dataProgroessLine[3].dataset.percent4;
+        for (let i = 0; i < skillsLine.length; i++) {
+
+            skillsLine[i].style.width = skillsLine[i].dataset.progress;
+        }
+    }
+
+    if (stats.getBoundingClientRect().top < 300 && !started) {
+
+        statsNumber.forEach((temp) => {
+            counter(temp);
+        });
+        started = true;
     }
 
 });
+
 
 
 
@@ -24,7 +46,6 @@ setInterval(function () {
     document.querySelector(".timeRemain--seconds .timeRemain--number").innerHTML = d.getSeconds();
     d.setSeconds(d.getSeconds() - 1);
 }, 1000);
-
 
 
 
